65ad3cfc4a03beac0b8b978099d14f22fe648d2d
Milan Dzodic <milan.dzodic@hotmail.com>
2025-05-14 21:53:22 +0200

    Halloj Hans

--- WebApp/Controllers/ProjectsController.cs
+++ WebApp/Controllers/ProjectsController.cs
@@ -1,12 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Models;
+using Business.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebApp.Controllers;
 
 [Route("projects")]
-public class ProjectsController : Controller
+public class ProjectsController(IProjectService projectService) : Controller
 {
-  public IActionResult Projects()
+  private readonly IProjectService _projectService = projectService;
+
+  public async Task<IActionResult> Index()
   {
-    return View();
+    var model = new ProjectsViewModel
+    {
+      Projects = await _projectService.GetProjectAsync()
+    };
+
+    return View(model);
+  }
+
+  [HttpPost]
+  public async Task<IActionResult> Add(AddProjectViewModel model)
+  {
+    var addProjectFormData = model.MapTo<AddProjectFormData>();
+    var result = await _projectService.CreateProjectAsync(addProjectFormData); return Json(new { });
+  }
+
+  [HttpPost]
+  public IActionResult Update(EditProjectViewModel model)
+  {
+    return Json(new { });
+  }
+
+  [HttpPost]
+  public IActionResult Delete(string id)
+  {
+    return Json(new { });
   }
 }
